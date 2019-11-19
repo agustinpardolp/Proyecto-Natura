36aa9b8842db121b66e14726c105b902323c32b8
agustinpardolp <agustinpardo@hotmail.com>
2019-11-19 12:52:52 -0300

    ok

--- back/db/index.js
+++ back/db/index.js
@@ -3,8 +3,8 @@ const db = new Sequelize('postgres://localhost:5432/naturaDb', {
     logging: false
 })
 
-// const db = new Sequelize('NaturaPrueba','root', 'Seem2Jump6', {
-//     host: 'nomaddsdb.cykssfqpmcmf.us-west-2.rds.amazonaws.com',
+// const db = new Sequelize('NaturaPrueba','root', 'XXXXXXXXXXJumpX', {
+//     host: 'XXXXX.cykssfqpmcmf.us-west-2.rds.amazonaws.com',
 //     port: 3306,
 //     dialect: 'mysql',
 //     dialectOptions: {
